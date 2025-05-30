bf89a7c33a893300b1ac65493d76644e2ca75540
Destan Tezel <destantezel@googlemail.com>
2025-05-30 22:10:49 +0100

    copy Supabase starter template

--- src/app/protected/layout.tsx
+++ src/app/protected/layout.tsx
@@ -1,17 +1,20 @@
-import { DeployButton } from "@/components/deploy-button";
-import { AuthButton } from "@/components/auth-button";
-import { Hero } from "@/components/hero";
-import { ThemeSwitcher } from "@/components/theme-switcher";
-import Link from "next/link";
+import { DeployButton } from '@/components/deploy-button';
+import { AuthButton } from '@/components/auth-button';
+import { ThemeSwitcher } from '@/components/theme-switcher';
+import Link from 'next/link';
 
-export default function Home() {
+export default function ProtectedLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <main className="flex min-h-screen flex-col items-center">
       <div className="flex w-full flex-1 flex-col items-center gap-20">
         <nav className="border-b-foreground/10 flex h-16 w-full justify-center border-b">
           <div className="flex w-full max-w-5xl items-center justify-between p-3 px-5 text-sm">
             <div className="flex items-center gap-5 font-semibold">
-              <Link href={"/"}>Next.js Supabase Starter</Link>
+              <Link href={'/'}>Next.js Supabase Starter</Link>
               <div className="flex items-center gap-2">
                 <DeployButton />
               </div>
@@ -20,12 +23,12 @@ export default function Home() {
           </div>
         </nav>
         <div className="flex max-w-5xl flex-1 flex-col gap-20 p-5">
-          <Hero />
+          {children}
         </div>
 
         <footer className="mx-auto flex w-full items-center justify-center gap-8 border-t py-16 text-center text-xs">
           <p>
-            Powered by{" "}
+            Powered by{' '}
             <a
               href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
               target="_blank"
